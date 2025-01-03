5b60f6d7dc10a997b99f30b5e95c73a9ac880b05
GoodDeveloper-coder <itmancik@gmail.com>
2025-01-03 19:32:37 +0200

    Added player knockback, player invulnerable and enemy Scriptable Object

--- BossRush2025/Assets/!!!Scripts/Prox/HealthManager.cs
+++ BossRush2025/Assets/!!!Scripts/Prox/HealthManager.cs
@@ -6,6 +6,8 @@ public class HealthManager : MonoBehaviour
     [SerializeField] private float _health = 10f;
     private float _maxHealth;
     private bool _isAlive = true;
+    private bool _receivceDamage = true;
+    public event Action _onHit;
     public event Action _onDie;
 
     void Start()
@@ -15,10 +17,11 @@ public class HealthManager : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        if (!_isAlive)
+        if (!_isAlive || !_receivceDamage)
             return;
 
         _health = Math.Clamp(_health - damage, 0, _maxHealth);
+        _onHit?.Invoke();
         if (_health == 0)
         {
             _onDie?.Invoke();
@@ -32,5 +35,23 @@ public class HealthManager : MonoBehaviour
             return;
 
         _health = Math.Clamp(_health + health, 0, _maxHealth);
+    }
+
+    public void SetHealth(float health)
+    {
+        if (!_isAlive)
+            return;
+
+        _health = Math.Clamp(health, 0, _maxHealth);
+    }
+
+    public void EnableReceivingDamage()
+    {
+        _receivceDamage = true;
+    }
+
+    public void DisableReceivingDamage()
+    {
+        _receivceDamage = false;
     }
 }
---
+++ BossRush2025/Assets/!!!Scripts/Prox/Knockback.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using System.Collections;
+
+public class Knockback : MonoBehaviour
+{
+    [Header("KnockBack Properties")]
+    [SerializeField] private float _knockBackStrength = 6f;
+    [SerializeField] private float _knockBackTime = 1f;
+
+    private Rigidbody2D _rb;
+    private bool _haveKnockback = false;
+    public event Action _onStartKnockback;
+    public event Action _onFinishKnockback;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void PlayKnockBack(Vector2 knockBackPoint)
+    {
+        if (_haveKnockback)
+            return;
+
+        _onStartKnockback?.Invoke();
+        _haveKnockback = true;
+
+        Vector2 position = transform.position;
+        Vector2 direction = (position - knockBackPoint).normalized;
+
+        _rb.AddForce(direction * _knockBackStrength, ForceMode2D.Impulse);
+        StartCoroutine(KnockBackDelay(_knockBackTime));
+    }
+
+    IEnumerator KnockBackDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        _onFinishKnockback?.Invoke();
+        _haveKnockback = false;
+    }
+}
