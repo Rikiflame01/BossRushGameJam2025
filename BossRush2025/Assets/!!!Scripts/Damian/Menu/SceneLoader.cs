c93be901e91034d8b3dc6f1428c1ec3418a7aa4e
GoodDeveloper-coder <itmancik@gmail.com>
2025-02-02 19:33:33 +0200

    Dummy scene UI input fix

--- BossRush2025/Assets/!!!Scripts/Damian/Menu/SceneLoader.cs
+++ BossRush2025/Assets/!!!Scripts/Damian/Menu/SceneLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,13 @@ public class SceneLoader : MonoBehaviour
     private int _startScene = 0;
     public void LoadScene()
     {
+        StartCoroutine(LoadSceneWithDelay());
+    }
+
+    private IEnumerator LoadSceneWithDelay()
+    {
+        yield return new WaitForSeconds(1);
+
         soundMenuManager.PlaySFX("Play");
         _startScene = PlayerPrefs.GetInt("StartScene", _startScene);
         if (_startScene == 0)
@@ -19,5 +27,4 @@ public class SceneLoader : MonoBehaviour
             SceneManager.LoadScene("GameplayScene");
         }
     }
-
 }
